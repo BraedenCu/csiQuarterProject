0bf76914573eb5c537f3bea0dba43bf5da826b96
BraedenCu <braedencullen120@gmail.com>
2021-02-22 13:18:16 -0600

    need to fix navbar centering issue

---
+++ js/fakescroll.js
@@ -0,0 +1,8 @@
+function smoothScroll(classId){
+    document.querySelector(classId).scrollIntoView({
+        behavior: 'smooth'
+    });
+    document.getElementById("pagetwo").style.opacity = 100;
+    document.getElementById("pagetwo").style.pointerEvents = 'auto';
+    
+}
